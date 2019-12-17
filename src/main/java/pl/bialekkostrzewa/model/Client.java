d3c0fd0beae7103a5a57a453cd210a3536acd291
Łukasz Kostrzewa <lukko98@gmail.com>
2019-12-17 13:34:42 +0100

    Update Clients type first concept

--- src/main/java/pl/bialekkostrzewa/model/Client.java
+++ src/main/java/pl/bialekkostrzewa/model/Client.java
@@ -3,6 +3,7 @@ package pl.bialekkostrzewa.model;
 public class Client extends User{
 
     private ClientType type;
+    private String typeName;
 
     public Client(){
         type = new NormalClient();
@@ -33,6 +34,29 @@ public class Client extends User{
 
     public ClientType getType() {
         return type;
+    }
+
+    public void setType(String type) {
+        switch (type){
+            case "Normal":
+                this.type = new NormalClient();
+                break;
+            case "Regular":
+                this.type = new RegularClient();
+                break;
+            case "Premium":
+                this.type = new PremiumClient();
+                break;
+        }
+    }
+
+    public String getTypeName(){
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+        setType(typeName);
     }
 
     public void setType(ClientType type) {
