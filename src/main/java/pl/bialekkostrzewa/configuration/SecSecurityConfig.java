79dd37399fb059c8409cd1e19b1a22c468f6877a
PawelBialek <216723@edu.p.lodz.pl>
2019-12-22 16:50:16 +0100

    Security initial commit

---
+++ src/main/java/pl/bialekkostrzewa/configuration/SecSecurityConfig.java
@@ -0,0 +1,53 @@
+package pl.bialekkostrzewa.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+        auth.authenticationProvider(authenticationProvider());
+    }
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/restaurant/reservations").access("hasRole('ROLE_USER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/restaurant/resources").access("hasRole('ROLE_MANAGER')")
+                .antMatchers("/restaurant/users").access("hasRole('ROLE_ADMIN')")
+                .and()
+                    .formLogin().loginPage("/loginPage")
+                    .defaultSuccessUrl("/homePage")
+                    .failureUrl("/loginPage?error")
+                    .usernameParameter("username").passwordParameter("password")
+                .and()
+                    .logout().logoutSuccessUrl("/loginPage?logout");
+
+    }
+
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
