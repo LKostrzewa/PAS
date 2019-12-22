5095fb7c8f442e679bdc5b6fc60813efba4b0725
Łukasz Kostrzewa <lukko98@gmail.com>
2019-12-22 21:48:25 +0100

    Something doesn't work big time

--- src/main/java/pl/bialekkostrzewa/controller/RegistrationController.java
+++ src/main/java/pl/bialekkostrzewa/controller/RegistrationController.java
@@ -1,17 +1,28 @@
 package pl.bialekkostrzewa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import pl.bialekkostrzewa.model.Client;
 import pl.bialekkostrzewa.service.UserService;
 
+import javax.validation.Valid;
+
 //@RequestMapping("")
 @Controller
 public class RegistrationController {
 
-    private UserService userService;
+    /*private UserService userService;
+
+    @Autowired
+    public RegistrationController(UserService userService){
+        this.userService = userService;
+    }*/
 
     @RequestMapping
     public String showMainPage(ModelMap model){
@@ -29,5 +40,15 @@ public class RegistrationController {
         Client client = new Client();
         client.setActive(false);
         return new ModelAndView("register", "user", client);
+    }
+
+    @PostMapping("/aaaa")
+    public String addClient(@Valid @ModelAttribute("user") Client client, BindingResult bindingResult){
+        /*if(!bindingResult.hasErrors()){
+            userService.addUser(client);
+            //for now
+            return "redirect:/users/";
+        }*/
+        return "register";
     }
 }
