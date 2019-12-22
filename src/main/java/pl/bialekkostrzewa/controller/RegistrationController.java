1fe1706ccaf5d301c54a618845a3088c3c4f4404
Łukasz Kostrzewa <lukko98@gmail.com>
2019-12-22 19:51:05 +0100

    Register page done

--- src/main/java/pl/bialekkostrzewa/controller/RegistrationController.java
+++ src/main/java/pl/bialekkostrzewa/controller/RegistrationController.java
@@ -4,10 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import pl.bialekkostrzewa.model.Client;
+import pl.bialekkostrzewa.service.UserService;
 
 //@RequestMapping("")
 @Controller
 public class RegistrationController {
+
+    private UserService userService;
 
     @RequestMapping
     public String showMainPage(ModelMap model){
@@ -22,6 +26,8 @@ public class RegistrationController {
 
     @RequestMapping("/register")
     public ModelAndView showRegisterPage(){
-        return null;
+        Client client = new Client();
+        client.setActive(false);
+        return new ModelAndView("register", "user", client);
     }
 }
