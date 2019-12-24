d4de40de25233d332936677b0c5a75a0b2ce6fe2
Łukasz Kostrzewa <lukko98@gmail.com>
2019-12-24 09:00:38 +0100

    Suggestion of solving a problem with getting a role from user

--- src/main/java/pl/bialekkostrzewa/converter/UserToSecurityUserConverter.java
+++ src/main/java/pl/bialekkostrzewa/converter/UserToSecurityUserConverter.java
@@ -3,10 +3,13 @@ package pl.bialekkostrzewa.converter;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import pl.bialekkostrzewa.model.Administrator;
+import pl.bialekkostrzewa.model.Manager;
 import pl.bialekkostrzewa.model.MyUserDetails;
 import pl.bialekkostrzewa.model.User;
 
 import java.util.Collections;
+import java.util.List;
 
 public class UserToSecurityUserConverter implements Converter<User, UserDetails> {
     @Override
@@ -15,8 +18,18 @@ public class UserToSecurityUserConverter implements Converter<User, UserDetails>
         userDetails.setUsername(user.getLogin());
         userDetails.setPassword(user.getPassword());
         userDetails.setEnabled(user.isActive());
+
         //TODO tu mamy problem bo trzeba ustawić role naszego usera - klient,admin,manager
         //userDetails.setAuthorities(Collections.singletonList(new SimpleGrantedAuthority()));
+        if(user instanceof Administrator){
+            userDetails.setAuthorities(List.of(new SimpleGrantedAuthority("ROLE_ADMIN")));
+        }
+        else if(user instanceof Manager){
+            userDetails.setAuthorities(List.of(new SimpleGrantedAuthority("ROLE_MANAGER")));
+        }
+        else {
+            userDetails.setAuthorities(List.of(new SimpleGrantedAuthority("ROLE_USER")));
+        }
         return userDetails;
     }
 }
