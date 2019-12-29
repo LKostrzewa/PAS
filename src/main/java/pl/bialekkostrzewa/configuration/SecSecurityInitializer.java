9f7f87a097f8dc03218c16561c492a9cda6880ef
PawelBialek <216723@edu.p.lodz.pl>
2019-12-29 14:35:40 +0100

    Missing files

---
+++ src/main/java/pl/bialekkostrzewa/configuration/SecSecurityInitializer.java
@@ -0,0 +1,6 @@
+package pl.bialekkostrzewa.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
